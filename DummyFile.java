1a167fec52c81feea629384bd5208c0e400c8927
codegurudummy <66222331+codegurudummy@users.noreply.github.com>
2020-07-30 10:25:40 -0400

    Update DummyFile.java

--- DummyFile.java
+++ DummyFile.java
@@ -1,4 +1,25 @@
+import com.amazonaws.services.kinesis.AmazonKinesisClient;
+import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+
+import example.foo.FooClient;
+
 public class DummyFile {
-    jkgfldksghkjfdhgkfdgkfdhsjnvjkfdnvkjfdnvkjf
-        gfdgdfjkgkdj
+    private AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
+    private AmazonKinesisClient kinesisClient = AmazonKinesisClientBuilder.defaultClient();
+    private FooClient fooClient;
+
+    // Should flag and suggest v2
+    public void s3ListObjects() {
+        s3Client.listObjects(new ListObjectsRequest());
+    }
+
+    // Should not flag
+    public void s3ListObjectsV2() {
+        s3Client.listObjectsV2(new ListObjectsV2Request());
+    }
 }
